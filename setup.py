6acea2b28e94ba63484bae69c1724235ce9c4b4d
Jafar Atili <at.jafar@outlook.com>
2022-04-11 22:05:24 +0300

    increased ver

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name = 'PySwitchbee',
     packages = ['switchbee'],
     install_requires=['requests', 'urllib3'],
-    version = '0.0.3',
+    version = '0.0.4',
     description = 'A library to communicate with SwitchBee',
     author='Jafar Atili',
     url='https://github.com/jafar-atili/pySwitchbee/',
